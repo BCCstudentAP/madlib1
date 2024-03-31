2c8291afd4a8e943c3f5c1b2cd5a9de11a47c208
Anna Phu <anna.phu.email@gmail.com>
2024-03-31 13:19:50 -0700

    The stuff required for assignment 4
    It has the two images, the html file, and the javascript.

---
+++ speech.js
@@ -0,0 +1,30 @@
+/* first prompt*/
+var alien_speech = prompt("What is your name?");
+
+document.getElementById("yourName").innerHTML = alien_speech;
+
+/*second_prompt*/
+var alien_speech = prompt("What is today's date?");
+
+document.getElementById("todayDate").innerHTML = alien_speech;
+
+/*third prompt*/
+var alien_speech = prompt("Give an alien a name.");
+
+document.getElementById("alienName").innerHTML = alien_speech;
+
+/*Forth prompt*/
+var alien_speech = prompt("Give me a name of a planet, real or imaginary");
+
+document.getElementById("planetName").innerHTML = alien_speech;
+
+
+/*Fifth prompt*/
+var alien_speech = prompt("Give me a different name of a planet, real or imaginary");
+
+document.getElementById("planetName2").innerHTML = alien_speech;
+
+/*Sixtth prompt*/
+var alien_speech = prompt("Type in a drink");
+
+document.getElementById("drink").innerHTML = alien_speech;
